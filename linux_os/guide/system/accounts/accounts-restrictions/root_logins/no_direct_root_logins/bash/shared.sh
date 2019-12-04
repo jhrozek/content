56f33828530df9989820340130dc65fb931281e0
Jakub Hrozek <jhrozek@redhat.com>
2019-12-04 10:35:38 +0100

    Add dummy remediation for empty securetty

--- linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_direct_root_logins/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-restrictions/root_logins/no_direct_root_logins/bash/shared.sh
@@ -1,2 +1,18 @@
 # platform = multi_platform_all
-echo > /etc/securetty
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: worker
+  name: 50-worker-empty-securetty
+spec:
+  config:
+    ignition:
+      version: 2.2.0
+    storage:
+      files:
+      - contents:
+          source: data:,
+        filesystem: root
+        mode: 0600
+        path: /etc/securetty
